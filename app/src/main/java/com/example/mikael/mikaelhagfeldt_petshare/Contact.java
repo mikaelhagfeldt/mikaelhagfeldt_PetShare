2fb827d3a6f3cb1dd70220959cb3c3394bc18123
mikaelhagfeldt <33085380+mikaelhagfeldt@users.noreply.github.com>
2018-03-14 12:41:27 +0100

    Test 2.

---
+++ app/src/main/java/com/example/mikael/mikaelhagfeldt_petshare/Contact.java
@@ -0,0 +1,17 @@
+package com.example.mikael.mikaelhagfeldt_petshare;
+
+/**
+ * Created by mikae on 2018-03-14.
+ */
+
+public class Contact
+{
+    private String fieldName;
+    private String fieldPhoneNumber;
+
+    public Contact(String fieldName, String fieldPhoneNumber)
+    {
+        this.fieldName = fieldName;
+        this.fieldPhoneNumber = fieldPhoneNumber;
+    }
+}
