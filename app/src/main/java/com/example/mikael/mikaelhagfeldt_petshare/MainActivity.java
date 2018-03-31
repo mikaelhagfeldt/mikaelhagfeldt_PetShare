9458c62e1f5c2c6bc6e7f10e1562d77910f4fee5
mikaelhagfeldt <33085380+mikaelhagfeldt@users.noreply.github.com>
2018-03-31 11:46:44 +0200

    Implementering av Firebase.
    Verkar som att allting fungerar nu.

--- app/src/main/java/com/example/mikael/mikaelhagfeldt_petshare/MainActivity.java
+++ app/src/main/java/com/example/mikael/mikaelhagfeldt_petshare/MainActivity.java
@@ -3,16 +3,23 @@ package com.example.mikael.mikaelhagfeldt_petshare;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity
 {
+    private FirebaseDatabase fieldDatabase;
+    private DatabaseReference fieldDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fieldDatabase = FirebaseDatabase.getInstance();
+        fieldDatabaseReference = fieldDatabase.getReference("Test Node");
+        fieldDatabaseReference.setValue("Test Value");
     }
 
-
-    // Test
 }
