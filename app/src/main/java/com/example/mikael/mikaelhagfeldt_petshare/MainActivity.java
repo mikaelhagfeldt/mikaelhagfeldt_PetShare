8cd1b23be99ff901c253bf66478f060eaeb3255c
mikaelhagfeldt <33085380+mikaelhagfeldt@users.noreply.github.com>
2018-03-31 12:05:42 +0200

    Design.
    Försöker komma upp med en simpel startdesign för den kommande presentationen.

--- app/src/main/java/com/example/mikael/mikaelhagfeldt_petshare/MainActivity.java
+++ app/src/main/java/com/example/mikael/mikaelhagfeldt_petshare/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.mikael.mikaelhagfeldt_petshare;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
-    private FirebaseDatabase fieldDatabase;
-    private DatabaseReference fieldDatabaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,9 +12,6 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fieldDatabase = FirebaseDatabase.getInstance();
-        fieldDatabaseReference = fieldDatabase.getReference("Test Node");
-        fieldDatabaseReference.setValue("Test Value");
     }
 
 }
