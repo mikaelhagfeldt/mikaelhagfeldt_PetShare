af6d784dca8b4c4bfad03742fc20826f3722413c
mikaelhagfeldt <33085380+mikaelhagfeldt@users.noreply.github.com>
2018-04-12 17:16:33 +0200

    Stor uppdatering.
    En stor klump kod på en och samma gång. Har tagit någon vecka att få allting att fungera, men nu verkar det som att appen inte kraschar längre och att de funktioner som hittills implementerats gör det de ska.

---
+++ app/src/main/java/com/example/mikael/mikaelhagfeldt_petshare/SubmitNewPost.java
@@ -0,0 +1,15 @@
+package com.example.mikael.mikaelhagfeldt_petshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubmitNewPost extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_new_post);
+    }
+}
